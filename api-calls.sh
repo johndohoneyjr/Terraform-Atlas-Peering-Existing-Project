8d12bc241771206c8e90579256492f1728f4b3c1
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2020-08-19 06:40:37 -0700

    Update api-calls.sh

--- api-calls.sh
+++ api-calls.sh
@@ -4,6 +4,6 @@
 https://cloud.mongodb.com/api/atlas/v1.0/groups/5d656831c56c98173cf5af4b/containers
 
 ## Get All Containers
-curl --user "eqsdyzkr:1afd74e0-5def-4f93-91ef-c2e0040574b9" --digest \
+curl --user "<public Key>:<private key>" --digest \
   --header "Accept: application/json" \
-  --request GET "https://cloud.mongodb.com/api/atlas/v1.0/groups/5d656831c56c98173cf5af4b/containers?providerName=AWS&pretty=true"
+  --request GET "https://cloud.mongodb.com/api/atlas/v1.0/groups/<group id>/containers?providerName=AWS&pretty=true"
